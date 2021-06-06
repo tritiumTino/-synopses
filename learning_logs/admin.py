e336d96c3ceb2112507b001737067db3fac2c7b1
tritium <tinotiano@gmail.com>
2021-06-06 21:29:12 +0300

    new aspects to models

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -3,5 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from .models import Topic, Entry
 
-admin.site.register(Topic)
-admin.site.register(Entry)
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('text', 'owner', 'date_added')
+    list_display_links = ('text',)
+    search_fields = ('text', 'owner',)
+
+
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('topic', 'text', 'date_added')
+    list_display_links = ('text',)
+    search_fields = ('text',)
+
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Entry, EntryAdmin)
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -5,9 +5,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Topic(models.Model):
     """the topic the user is studying"""
-    text = models.CharField(max_length=200)
-    date_added = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200, verbose_name='Тема')
+    date_added = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Создатель')
+
+    class Meta:
+        verbose_name_plural = 'Темы'
+        verbose_name = 'Тема'
+        ordering = ['-date_added']
 
     def __str__(self):
         return self.text
@@ -15,15 +20,16 @@ class Topic(models.Model):
 
 class Entry(models.Model):
     """the information learned by the user on the topic"""
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    text = models.TextField(default='')
-    date_added = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, verbose_name='Тема')
+    text = models.TextField(default='', verbose_name='Содержание')
+    date_added = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
 
     class Meta:
-        verbose_name_plural = 'entries'
+        verbose_name_plural = 'Записи'
+        verbose_name = 'Запись'
+        ordering = ['-date_added']
 
     def __str__(self):
         if len(self.text) > 50:
             return f'{self.text[:50]}...'
         return self.text
-
